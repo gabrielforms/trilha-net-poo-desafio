8fceefed02cf586ea8ceaa5efa6fa703936a49d9
gabrielforms <gabrielformiga3@gmail.com>
2024-01-08 18:54:17 -0300

    resolução do desafio proposto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿using DesafioPOO.Models;
+﻿﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main() 
+    {
+        Nokia nokiaPhone = new Nokia(numero: "95012001", modelo: "Nokia GC60", imei: "IMEI2011", memoria: 256);
+        nokiaPhone.Ligar();
+        nokiaPhone.ReceberLigacao();
+        nokiaPhone.InstalarAplicativo("Github");
+
+        Iphone iphone = new Iphone("88912419", "iPhone 14", "IMEI5512", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("StackOverflow");
+    }
+
+}
